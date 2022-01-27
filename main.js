3e4d189a17df16130aa5e687c40a260886b99beb
tmz81 <tmz.zt4@gmail.com>
2022-01-27 11:53:30 -0300

    🚀

--- main.js
+++ main.js
@@ -74,8 +74,9 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__header, .section__title`,{delay: 600})
-sr.reveal(`.home__footer`,{delay: 700})
-sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
+sr.reveal(`.home__footer`,{delay: 600})
+sr.reveal(`.home__img`,{delay: 600, origin: 'top'})
+sr.reveal(`.video__play`, {delay: 600})
 
 sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
 sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
